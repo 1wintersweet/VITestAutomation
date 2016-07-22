adad686a8895f06523b7a2008619b9983750f63f
ge <ge>
2016-07-21 17:33:03 -0700

    First commit

---
+++ EmployeeTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace EmployeeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string url = "https://dev.viditure.com/beta";
+
+        private IWebDriver _driver;
+
+        [TestMethod]
+        public void Test1()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(url);
+            ClickLoginLink();
+        }
+
+        public void ClickLoginLink()
+        {
+            _driver.FindElement(By.LinkText("Login")).Click();
+          
+
+        }
+    }
+}
